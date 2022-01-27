92bbb8cac600d2b2c65e53735feebf27633c45f7
Kyle Betts <kyle.betts@cdw.com>
2022-01-27 17:10:32 +0000

    fixed vnet cidr

--- network.tf
+++ network.tf
@@ -3,8 +3,8 @@ module "network" {
   version = "3.5.0"
   
   resource_group_name = "kylebetts-workshop"
-  address_spaces      = ["10.0.10.0/16"]
-  subnet_prefixes     = ["10.0.10.0/24"]
+  address_spaces      = ["10.0.10.0/24"]
+  subnet_prefixes     = ["10.0.10.0/25"]
   subnet_names        = ["chip-subnet"]
   subnet_service_endpoints = {
     "chip-subnet" : ["Microsoft.Sql"]
