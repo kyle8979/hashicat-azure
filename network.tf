8540a866a855d8eeea43a4c03585d59305d05dc6
kyle8979 <58950836+kyle8979@users.noreply.github.com>
2022-01-27 10:59:15 -0600

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,18 @@
+module "network" {
+  source  = "app.terraform.io/chip-cert/network/azurerm"
+  version = "3.5.0"
+  
+  resource_group_name = "kylebetts-workshop
+  address_spaces      = ["10.0.10.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_names        = ["chip-subnet"]
+
+  subnet_service_endpoints = {
+    "chip-subnet" : ["Microsoft.Sql"]
+  }
+
+  tags = {
+    Department = "dev"
+    Billable  = "true"
+  }
+}
